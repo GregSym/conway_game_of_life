4c607e63d300c02c5fbe96540815cc2bbf467b23
GT Symington <gtsrex@gmail.com>
2021-05-20 16:27:46 +0200

    updated naming conventions

--- lib/src/game_objects/gol_truths.dart
+++ lib/src/game_objects/gol_truths.dart
@@ -3,17 +3,18 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 
 class GoLTruths with ChangeNotifier {
-  List<List<bool>> _truths = [];
+  List<List<bool>> _cells = [];
+  List<List<bool>> _nextIterationCells = [];
   bool _ready = false;
   Timer updateTimer;
 
   // starting dimensions
   int _width = 10;
-  int _height = 32;
+  int _height = 10;
 
   // getters
-  int get crossAxis => _truths[0].length;
-  List<List<bool>> get truths => _truths;
+  int get crossAxis => _cells[0].length;
+  List<List<bool>> get truths => _cells;
   int get totalAlive => _tallyTruths();
   bool get isReady => _ready;
 
@@ -21,7 +22,7 @@ class GoLTruths with ChangeNotifier {
   set toggleCell(CellLocation loc) => _toggleCell(loc);
 
   _toggleCell(CellLocation loc) {
-    _truths[loc.row][loc.col] = !_truths[loc.row][loc.col];
+    _cells[loc.row][loc.col] = !_cells[loc.row][loc.col];
     notifyListeners();
   }
 
@@ -29,11 +30,11 @@ class GoLTruths with ChangeNotifier {
   initGame() {
     for (int j = 0; j < _height; j++) {
       // set height
-      _truths.add([]);
+      _cells.add([]);
     }
     for (int i = 0; i < _width; i++) {
       // set width
-      for (List<bool> row in _truths) {
+      for (List<bool> row in _cells) {
         row.add(false);
       }
     }
@@ -42,7 +43,7 @@ class GoLTruths with ChangeNotifier {
   }
 
   resetGame() {
-    _truths.clear();
+    _cells.clear();
     updateTimer.cancel();
     initGame();
   }
@@ -58,41 +59,46 @@ class GoLTruths with ChangeNotifier {
   }
 
   void _updateRows() {
-    for (int i = 1; i < _truths.length - 1; i++) {
-      for (int j = 1; j < _truths[0].length - 1; j++) {
+    _nextIterationCells =
+        List.from(_cells); // get the last grid of cells to work off of
+    for (int i = 1; i < _cells.length - 1; i++) {
+      for (int j = 1; j < _cells[0].length - 1; j++) {
         _updateCell(i, j);
       }
     }
+    //if (_cells.isNotEmpty) _cells.clear();
+    _cells = List.from(
+        _nextIterationCells); // update current cells all at once from a new layer
   }
 
   void _updateCell(int i, int j) {
     int aliveNeighbours = 0;
-    bool currentCell = _truths[i][j];
+    bool currentCell = _cells[i][j];
     List<bool> neighbours = [
-      _truths[i - 1][j],
-      _truths[i - 1][j - 1],
-      _truths[i - 1][j + 1],
-      _truths[i][j - 1],
-      _truths[i][j + 1],
-      _truths[i + 1][j],
-      _truths[i + 1][j - 1],
-      _truths[i + 1][j + 1], // needs to have 8 neighbours
+      _cells[i - 1][j],
+      _cells[i - 1][j - 1],
+      _cells[i - 1][j + 1],
+      _cells[i][j - 1],
+      _cells[i][j + 1],
+      _cells[i + 1][j],
+      _cells[i + 1][j - 1],
+      _cells[i + 1][j + 1], // needs to have 8 neighbours
     ];
     for (bool neighbour in neighbours) if (neighbour) aliveNeighbours++;
     if (!currentCell && aliveNeighbours == 3) {
-      _truths[i][j] = true; // dead comes alive
+      _nextIterationCells[i][j] = true; // dead comes alive
     } else if (currentCell) {
       if (aliveNeighbours == 2 || aliveNeighbours == 3)
-        _truths[i][j] = true; // alive stays alive
+        _nextIterationCells[i][j] = true; // alive stays alive
       else
-        _truths[i][j] = false; // dead cell condition
+        _nextIterationCells[i][j] = false; // dead cell condition
     } else {
-      _truths[i][j] = false; // dead cell condition
+      _nextIterationCells[i][j] = false; // dead cell condition
     }
   }
 
   expandWidth() {
-    for (List<bool> row in _truths) {
+    for (List<bool> row in _cells) {
       row.insert(0, false);
       row.add(false);
     }
@@ -100,32 +106,32 @@ class GoLTruths with ChangeNotifier {
   }
 
   expandHeight() {
-    _truths.insert(0, []);
-    _truths.add([]);
-    for (bool _ in _truths[1]) {
-      _truths[0].add(false);
-      _truths[_truths.length].add(false);
+    _cells.insert(0, []);
+    _cells.add([]);
+    for (bool _ in _cells[1]) {
+      _cells[0].add(false);
+      _cells[_cells.length].add(false);
     }
     notifyListeners();
   }
 
   expandSymmetric() {
-    for (List<bool> row in _truths) {
+    for (List<bool> row in _cells) {
       row.insert(0, false);
       row.add(false);
     }
-    _truths.insert(0, []);
-    _truths.add([]);
-    for (bool _ in _truths[1]) {
-      _truths[0].add(false);
-      _truths[_truths.length].add(false);
+    _cells.insert(0, []);
+    _cells.add([]);
+    for (bool _ in _cells[1]) {
+      _cells[0].add(false);
+      _cells[_cells.length].add(false);
     }
     notifyListeners(); // worried about calling notifyListeners while notifying listeners so rewriting the contents for this one
   }
 
   _tallyTruths() {
     int total = 0;
-    for (List<bool> row in _truths) {
+    for (List<bool> row in _cells) {
       for (bool cell in row) {
         if (cell) total++;
       }
