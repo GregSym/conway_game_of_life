fc0d1e003e45d50988ae563356d0b3bf2e16ba1c
GT Symington <gtsrex@gmail.com>
2021-05-24 00:42:45 +0200

    added more padding to the subsidiary screens

--- lib/src/screens/about_this_app.dart
+++ lib/src/screens/about_this_app.dart
@@ -5,27 +5,30 @@ class AboutThisApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              "About this App:",
-            ),
-            Text(
-                "This is a demonstration of Conway's Game of Life, a mathematical model of cellular automata"),
-            Text(
-                "Each cell is governed by a set of rules based on its relationship to surrounding cells."),
-            Text("The rules may be simplified as follows: "),
-            RuleText(
-                ruleText:
-                    "Any dead cell comes alive when neighbouring 3 alive cells"),
-            RuleText(
-                ruleText:
-                    "Living cells surrounded by either 2 or 3 alive neighbouring cells remain alive"),
-            RuleText(
-                ruleText:
-                    "All other cells will be dead in the next iteration of the Game"),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "About this App:",
+              ),
+              Text(
+                  "This is a demonstration of Conway's Game of Life, a mathematical model of cellular automata"),
+              Text(
+                  "Each cell is governed by a set of rules based on its relationship to surrounding cells."),
+              Text("The rules may be simplified as follows: "),
+              RuleText(
+                  ruleText:
+                      "Any dead cell comes alive when neighbouring 3 alive cells"),
+              RuleText(
+                  ruleText:
+                      "Living cells surrounded by either 2 or 3 alive neighbouring cells remain alive"),
+              RuleText(
+                  ruleText:
+                      "All other cells will be dead in the next iteration of the Game"),
+            ],
+          ),
         ),
       ),
     );
